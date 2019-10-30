d7edc1136bcb2f984f55e6b3bda95a387c491fd6
Byeongjin Kang <jasonkang14@gmail.com>
2019-10-30 21:54:28 +0900

    Add: comments

--- test-babel-custom-plugin/plugins/remove-log.js
+++ test-babel-custom-plugin/plugins/remove-log.js
@@ -1,15 +1,20 @@
 module.exports = function({ types: t }) {
     return {
         visitor: {
-            ExpressionStatement(path) {
+            ExpressionStatement(path) { 
+                // adding a method to be called once Expression Statement node is made
                 if (t.isCallExpression(path.node.expression)) {
+                    // check if the expression property of the ExpressionStatement is CallExpression
                     if (t.isMemberExpression(path.node.expression.callee)) {
+                        // check if the callee property is MemberExpression 
                         const memberExp = path.node.expression.callee;
                         if (
                             memberExp.object.name === 'console' &&
                             memberExp.property.name === 'log'
+                            // check if the log method of the console object is called
                         ) {
                             path.remove();
+                            // if all is fulfilled, remove the ExpressionStatement from AST
                         }
                     }
                 }
